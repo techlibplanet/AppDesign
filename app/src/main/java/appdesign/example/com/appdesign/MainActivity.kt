561de604c726947d0662f4516f52276fa26f043e
techlibplanet <techlibplanet@gmail.com>
2019-01-01 22:41:16 +0530

    Initial commit

---
+++ app/src/main/java/appdesign/example/com/appdesign/MainActivity.kt
@@ -0,0 +1,21 @@
+package appdesign.example.com.appdesign
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var signInButton : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        signInButton.findViewById<Button>(R.id.signInButton)
+
+        signInButton.setOnClickListener{
+
+        }
+    }
+}
---
+++ app/src/main/java/appdesign/example/com/appdesign/TransferPoints.kt
@@ -0,0 +1,12 @@
+package appdesign.example.com.appdesign
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TransferPoints : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transfer_points)
+    }
+}
